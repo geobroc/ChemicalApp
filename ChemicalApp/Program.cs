c58335fd0516f0cc77fb359de08d0d4b59d6a68d
Georgia Brockie <geobroc@chbc.school.nz>
2023-04-04 13:27:36 +1200

    Completed invalid data testing for OneEmployee()

--- ChemicalApp/Program.cs
+++ ChemicalApp/Program.cs
@@ -7,7 +7,10 @@ namespace ChemicalApp
     {
         // global variables
         static List<string> chemicals = new List<string>() { "Cyanide", "Propane", "Alcohol", "Chlorine" };
+        static int flag = 0;
+
         // methods and\or functions
+
         // checks if an integer entered by a user is within a specific range
         static int CheckInt(string question, int min, int max)
         {
@@ -15,13 +18,21 @@ namespace ChemicalApp
 
             while (true)
             {
-                int userInt = Convert.ToInt32(Console.ReadLine());
-
-                if (userInt >= min && userInt <= max)
+                try
                 {
-                    return userInt;
+                    int userInt = Convert.ToInt32(Console.ReadLine());
+
+                    if (userInt >= min && userInt <= max)
+                    {
+                        return userInt;
+                    }
+                    Console.WriteLine($"Error: Enter a number between {min} and {max}");
                 }
-                Console.WriteLine($"Error: Enter a number between {min} and {max}");
+                catch
+                {
+                    Console.WriteLine($"Error: Enter a number between {min} and {max}");
+                }
+                
             }
 
         }
@@ -33,47 +44,54 @@ namespace ChemicalApp
             int chemical = CheckInt("Choose a chemical:\n" +
                 "1. Cyanide\n" +
                 "2. Propane\n" +
-                "3. Alcohol\n" +
+                "3. Alcohol\n" + 
                 "4. Chlorine\n" +
                 "5. Quit\n", 1, 5);
 
-            float sumEfficiencies = 0;
-            // repeat the test 5 times
-            for (int i = 0; i < 5; i++)
+            
+
+            if (chemical == 5)
             {
-                // Enter and store amount of germs in sample
-
-                int germCount = CheckInt("Please enter an initial germ count in a sample", 100, 9000);
-
-                // Display an amount of time passed
-                Console.WriteLine("--- 45 minutes passed ---");
-
-                // Find amount of germs after passed time
-                Random randGerm = new Random();
-                int afterGermCount = randGerm.Next(0, germCount);
-
-                // Determine the efficiency of the chemical
-                float efficiency = (float)(germCount - afterGermCount) / 45;
-
-                sumEfficiencies = sumEfficiencies + efficiency;
+                flag = 5;
             }
-            // calculate the average which will be the final efficiency rating
-            float finalEficiency = (float)sumEfficiencies / 5;
-            finalEficiency = (float)Math.Round(finalEficiency, 2);
+            else
+            {
+                float sumEfficiencies = 0;
+                // repeat the test 5 times
+                for (int i = 0; i < 5; i++)
+                {
+                    // Enter and store amount of germs in sample
 
-            // Display and its final efficiency rating
-            Console.WriteLine($"Chemical {chemicals[chemical-1]} has an efficiency rating of {finalEficiency}.");
+                    int germCount = CheckInt("Please enter an initial germ count in a sample", 100, 9000);
+
+                    // Display an amount of time passed
+                    Console.WriteLine("--- 45 minutes passed ---");
+
+                    // Find amount of germs after passed time
+                    Random randGerm = new Random();
+                    int afterGermCount = randGerm.Next(0, germCount);
+
+                    // Determine the efficiency of the chemical
+                    float efficiency = (float)(germCount - afterGermCount) / 45;
+
+                    sumEfficiencies = sumEfficiencies + efficiency;
+                }
+                // calculate the average which will be the final efficiency rating
+                float finalEficiency = (float)sumEfficiencies / 5;
+                finalEficiency = (float)Math.Round(finalEficiency, 2);
+
+                // Display and its final efficiency rating
+                Console.WriteLine($"Chemical {chemicals[chemical - 1]} has an efficiency rating of {finalEficiency}.");
+            }
         }
 
         static void Main(string[] args)
         {
             // Loop OneChemical() until the user has tested all their chemicals
-            string flag = "";
-            while (!flag.Equals("XXX"))
+            
+            while (flag != 5)
             {
                 OneChemical();
-                Console.WriteLine("Press <Enter> to add another chemical or type 'XXX' to end");
-                flag = Console.ReadLine();
             }
             
             // Display the most and least effective chemical
